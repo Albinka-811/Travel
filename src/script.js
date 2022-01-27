a2ea2394af4c8f091ecb884e1655cd92e64a34ae
Albina Holovach <albinkasolodenko@gmail.com>
2022-01-27 20:16:21 +0200

    Correct the code js, fixed code scss and fixed code in html

--- src/script.js
+++ src/script.js
@@ -1,14 +1,13 @@
 function myFunction () {
     let closed = false
-    let blocks = [...document.querySelectorAll('.pictures-column')]
-    let btn = document.getElementById('pictures-toggle')
+    const blocks = [...document.querySelectorAll('.pictures-column')]
+    const btn = document.getElementById('pictures-toggle')
 
     btn.addEventListener('click', function () {
         for (let elem of blocks.slice(1)) {
             elem.classList.toggle('hide', closed)
         }
         closed = !closed
-        console.log(closed)
     })
 }
 
